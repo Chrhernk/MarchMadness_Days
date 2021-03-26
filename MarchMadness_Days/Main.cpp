0a8a8dd4df302b8c444acb250a32c4e7612490b5
CHRISTOPHER HERNKE <CHRHERNK@uat.edu>
2021-03-25 19:00:07 -0500

    Add project files.

---
+++ MarchMadness_Days/Main.cpp
@@ -0,0 +1,227 @@
+#include<iostream>
+#include<string>
+#include<cmath>
+
+void MarchDay1();
+void MarchDay2();
+void MarchDay3();
+void MarchDay4();
+void MarchDay5();
+void MarchDay6();
+void MarchDay7();
+void MarchDay8();
+void MarchDay9();
+void MarchDay10();
+void MarchDay11();
+void MarchDay12();
+void MarchDay13();
+void MarchDay14();
+void MarchDay15();
+void MarchDay16();
+void MarchDay17();
+void MarchDay18();
+void MarchDay19();
+void MarchDay20();
+void MarchDay21();
+void MarchDay22();
+void MarchDay23();
+void MarchDay24();
+void MarchDay25();
+void MarchDay26();
+void MarchDay27();
+void MarchDay28();
+void MarchDay29();
+void MarchDay30();
+void MarchDay31();
+
+
+int main()
+{
+	std::cout << "*** Welcome to a new adventure! Today, we will be leading you " << std::endl;
+	std::cout << "through the month of march, and its everyday holidays! ***" << std::endl;
+
+	std::cout << "But here, Before we begin, Why dont we go through a few facts of the month!" << std::endl;
+	std::cout << "\n\n" << std::endl;
+	std::cout << "March is the month named after the roman god, Mars! \n" << std::endl;
+	std::cout << "*** The month is the following month-long celebrations! ***\n" << std::endl;
+	std::cout << " 1 : Irish American Heritage Month" << std::endl;
+	std::cout << " 2 : Music in Our Schools Month" << std::endl;
+	std::cout << " 3 : National Celery Month" << std::endl;
+	std::cout << " 4 : National Craft Month" << std::endl;
+	std::cout << " 5 : National Frozen Food Month" << std::endl;
+	std::cout << " 6 : National Nutrition Month" << std::endl;
+	std::cout << " 7 : National Peanut Month" << std::endl;
+	std::cout << " 8 : National Women's History Month" << std::endl;
+	std::cout << " 9 : Red Cross Month" << std::endl;
+	std::cout << "10 : Social Workers Month" << std::endl;
+	std::cout << "\n\n" << std::endl;
+
+	std::cout << " *** These are the following week-long celebrations! ***\n\n" << std::endl;
+
+	std::cout << "Celebrate Your Name Week - 1 st full week of March" << std::endl;
+	std::cout << "2 nd Week National Bubble Week" << std::endl;
+	std::cout << "2 nd Week Crochet Week" << std::endl;
+
+	std::cout << "\n\n" << std::endl;
+
+	std::cout << " *** These are all of the facts we have for you! Enjoy your adventure! ***\n" << std::endl;
+
+	system("pause");
+
+}
+
+void MarchDay1()
+{
+
+
+}
+void MarchDay2()
+{
+
+
+}
+void MarchDay3()
+{
+
+
+}
+void MarchDay4()
+{
+
+
+}
+void MarchDay5()
+{
+
+
+}
+void MarchDay6()
+{
+
+
+}
+void MarchDay7()
+{
+
+
+}
+void MarchDay8()
+{
+
+
+}
+void MarchDay9()
+{
+
+
+}
+void MarchDay10()
+{
+
+
+}
+void MarchDay11()
+{
+
+
+}
+void MarchDay12()
+{
+
+
+}
+void MarchDay13()
+{
+
+
+}
+void MarchDay14()
+{
+
+
+}
+void MarchDay15()
+{
+
+
+}
+void MarchDay16()
+{
+
+
+}
+void MarchDay17()
+{
+
+
+}
+void MarchDay18() 
+{
+
+
+}
+void MarchDay19()
+{
+
+
+}
+void MarchDay20()
+{
+
+
+}
+void MarchDay21()
+{
+
+
+}
+void MarchDay22()
+{
+
+
+}
+void MarchDay23()
+{
+
+
+}
+void MarchDay24()
+{
+
+
+}
+void MarchDay25()
+{
+
+
+}
+void MarchDay26()
+{
+
+
+}
+void MarchDay27()
+{
+
+
+}
+void MarchDay28()
+{
+
+
+}
+void MarchDay29()
+{
+
+
+}
+void MarchDay30()
+{
+
+
+}
+void MarchDay31()
+{
+
+
+}
